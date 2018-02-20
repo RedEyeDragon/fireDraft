ef104a7ea1a599dcb3313583743dff058955c332
RedEyeDragon <bjduarte@asu.edu>
2018-02-20 22:46:43 +0000

    TCP, UDP, IP

--- reflector.py
+++ reflector.py
@@ -18,64 +18,96 @@ conf.iface = args.interface
 # monitor the arp requests
 def reflect_pkt (pkt):
    #pkt.show2()
-   if pkt.haslayer(ARP) and pkt[ARP].pdst == args.victim_ip:
-      arppkt = eval(pkt.command())
-      arppkt[Ether].src = args.victim_ethernet
-      arppkt[Ether].dst = pkt[Ether].src
-      arppkt[ARP].psrc = args.victim_ip
-      arppkt[ARP].hwsrc = args.victim_ethernet
-      arppkt[ARP].pdst = pkt[ARP].psrc
-      arppkt[ARP].hwdst = pkt[ARP].hwsrc
-      arppkt[ARP].op = 2
 
-      sendp(arppkt)
-      arppkt.show2()
-   elif pkt.haslayer(ARP) and pkt[ARP].pdst == args.reflector_ip:
-      arppkt = eval(pkt.command())
-      arppkt[Ether].src = args.reflector_ethernet
-      arppkt[Ether].dst = pkt[Ether].src
-      arppkt[ARP].psrc = args.reflector_ip
-      arppkt[ARP].hwsrc = args.reflector_ethernet
-      arppkt[ARP].pdst = pkt[ARP].psrc
-      arppkt[ARP].hwdst = pkt[ARP].hwsrc
-      arppkt[ARP].op = 2
+# Check to see if has ARP layer
+   if pkt.haslayer(ARP):
 
-      sendp(arppkt)
-      arppkt.show2()
+      if pkt[ARP].pdst == args.victim_ip:
+         arppkt = eval(pkt.command())
+         arppkt[Ether].src = args.victim_ethernet
+         arppkt[Ether].dst = pkt[Ether].src
+         arppkt[ARP].psrc = args.victim_ip
+         arppkt[ARP].hwsrc = args.victim_ethernet
+         arppkt[ARP].pdst = pkt[ARP].psrc
+         arppkt[ARP].hwdst = pkt[ARP].hwsrc
+         arppkt[ARP].op = 2
 
-# IP layer
-   if pkt.haslayer(IP) and pkt[IP].dst == args.victim_ip:
+         sendp(arppkt)
+         #arppkt.show2()
+
+
+      elif pkt[ARP].pdst == args.reflector_ip:
+         arppkt = eval(pkt.command())
+         arppkt[Ether].src = args.reflector_ethernet
+         arppkt[Ether].dst = pkt[Ether].src
+         arppkt[ARP].psrc = args.reflector_ip
+         arppkt[ARP].hwsrc = args.reflector_ethernet
+         arppkt[ARP].pdst = pkt[ARP].psrc
+         arppkt[ARP].hwdst = pkt[ARP].hwsrc
+         arppkt[ARP].op = 2
+
+         sendp(arppkt)
+         print("ARP sent packet: ")
+         #arppkt.show2()
+
+
+# Handle IP and Ethernet layers
+   else:
       ippkt = eval(pkt.command())
-         # Handle TCP
-      if pkt.haslayer(TCP) and pkt[IP].dst == args.victim_ip:
-         del ippkt[TCP].chksum
-      if pkt.haslayer(UDP) and pkt[IP].dst == args.victim_ip:
-         del ippkt[UDP].chksum
-      if pkt.haslayer(ICMP) and pkt[IP].dst == args.victim_ip:
-         del ippkt[ICMP].chksum
 
-# IP for Reflector
-   if pkt.haslayer(IP) and pkt[IP].dst == args.reflector_ip:
-      ippkt = eval(pkt.command())
-         # Handle TCP
-      if pkt.haslayer(TCP) and pkt[IP].dst == args.reflector_ip:
-         del ippkt[TCP].chksum
-      if pkt.haslayer(UDP) and pkt[IP].dst == args.reflector_ip:
-         del ippkt[UDP].chksum
-      if pkt.haslayer(ICMP) and pkt[IP].dst == args.reflector_ip:
-         del ippkt[ICMP].chksum
+# IP check
+      if pkt.haslayer(IP):
 
-# create packet and send
-      ippkt[Ether].src = args.reflector_ethernet
-      ippkt[Ether].dst = pkt[Ether].src
-      ippkt[IP].src = args.reflector_ip
-      ippkt[IP].dst = pkt[IP].src
-      del ippkt[IP].chksum
-      ippkt.show2()
+# Check if victim is destination
+         if pkt[IP].dst == args.victim_ip:
+            ippkt[Ether].src = args.reflector_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[IP].chksum
 
-      send(ippkt)
-      
 
+# Check if reflector is destination
+         elif pkt[IP].dst == args.reflector_ip:
+            ippkt[Ether].src = args.victim_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[IP].chksum
+
+
+# Check if has Ethernet layer
+      elif pkt.haslayer(TCP):
+
+         if pkt[TCP].dst == args.victim_ethernet:
+            ippkt[Ether].src = args.reflector_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[Ether/TCP].chksum
+
+
+         if pkt[TCP].dst == reflector_ethernet:
+            ippkt[Ether].src = args.victim_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[Ether/TCP].chksum
+
+
+# Check if has UDP layer
+      elif pkt.haslayer(UDP):
+
+         if pkt[UDP].dst == args.victim_ethernet:
+            ippkt[Ether].src = args.reflector_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[Ether/UDP].chksum
+
+         if pkt[UDP].dst == args.reflector_ethernet:
+            ippkt[Ether].src = args.victim_ethernet
+            ippkt[Ether].dst = pkt[Ether].src
+            del ippkt[Ether/UDP].chksum
+
+
+# create reflected packet and send back
+            ippkt[IP].src = args.reflector_ip
+            ippkt[IP].dst = pkt[IP].src
+            ippkt.show2()
+
+            print ("IP reply with new checksum")
+            send(ippkt)
 
 sniff(iface=args.interface, prn=reflect_pkt, store=0)
 
